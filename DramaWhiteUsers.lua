fec3625cdedc22120664aea20ff9ce83e0b8d555
7rab <119785971+lmaooa1043@users.noreply.github.com>
2024-06-14 13:21:51 +0300

    Create DramaWhiteUsers.lua

---
+++ DramaWhiteUsers.lua
@@ -0,0 +1,20 @@
+--[[
+
+Drama Whitelist
+
+Whitelisted UserIDS and Usernames,
+Dont think your a skidlord supreme by leaking this, if i really wanted to hide it you wouldnt find it anywhere
+
+]]
+
+return {
+    whitelistedUsernames = {
+        ["T_azb"] = true,
+        ["MoonlitMaple"] = true,
+        ["smirtuar"] = true,
+        ["RyosUser"] = true,
+        ["dzg_k"] = true,
+        ["Pyrosfere"] = true,
+        ["Elitetracks"] = true,
+    }
+}
