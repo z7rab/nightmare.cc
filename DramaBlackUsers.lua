416b26d9fa591691b451175e641dac77e91e31ec
7rab <119785971+z7rab@users.noreply.github.com>
2024-06-17 10:23:56 +0300

    Update DramaBlackUsers.lua

--- DramaBlackUsers.lua
+++ DramaBlackUsers.lua
@@ -1,5 +1,5 @@
 return {
-    whitelistedUsernames = {
+    blacklistedUsernames = {
         ["1x1x1Ix1xIxIx1"] = true,
         ["keyuser74747474"] = true,
         ["Dark_Monster998ksks"] = true,
