d6aa098e4cfd74268f68751dc36811b865f0ef59
7rab <119785971+z7rab@users.noreply.github.com>
2024-06-30 02:13:55 +0300

    Remove Maple from WL

--- PaidDramaUsers.lua
+++ PaidDramaUsers.lua
@@ -1,6 +1,6 @@
 [
     "T_azb", "smirtuar", "Fatedsky", "wll_sportz", "dzg_k", "Pyrosfere", 
-    "Elitetracks", "MoonlitMaple", "baconhackoff", "micmalsalts", 
+    "Elitetracks", "baconhackoff", "micmalsalts", 
     "WackyNoodleDoodle", "itsme_bombie", "ujonyavfdersgy", "krenchcob", 
     "RyosUser", "getyourprize550", "7rab_pWKyXJ52Gb3HDJ2", "user921920"
 ]
