4d9b8185b2dd9999c5964ebdb66aa45c8931523e
7rab <119785971+z7rab@users.noreply.github.com>
2024-07-01 01:18:06 +0300

    js removed my alt cuz buggy

--- PaidDramaUsers.lua
+++ PaidDramaUsers.lua
@@ -2,5 +2,5 @@
     "T_azb", "smirtuar", "Fatedsky", "wll_sportz", "dzg_k", "Pyrosfere", 
     "Elitetracks", "baconhackoff", "micmalsalts", 
     "WackyNoodleDoodle", "itsme_bombie", "ujonyavfdersgy", "krenchcob", 
-    "RyosUser", "getyourprize550", "7rab_pWKyXJ52Gb3HDJ2", "user921920"
+    "RyosUser", "7rab_pWKyXJ52Gb3HDJ2", "user921920"
 ]
