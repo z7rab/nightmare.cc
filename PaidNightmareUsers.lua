4ac6aaf293634b5607c5096d39955764543fd085
7rab <119785971+z7rab@users.noreply.github.com>
2024-07-08 11:16:57 +0300

    Update PaidNightmareUsers.lua

--- PaidNightmareUsers.lua
+++ PaidNightmareUsers.lua
@@ -1,6 +1,1 @@
-[
-    "T_azb", "smirtuar", "Fatedsky", "wll_sportz", "dzg_k", "Pyrosfere", 
-    "Elitetracks", "baconhackoff", "micmalsalts", 
-    "WackyNoodleDoodle", "itsme_bombie", "ujonyavfdersgy", "krenchcob", 
-    "RyosUser", "7rab_pWKyXJ52Gb3HDJ2", "user921920", "ImAm4t3ur"
-]
+haha nice try skibidi
